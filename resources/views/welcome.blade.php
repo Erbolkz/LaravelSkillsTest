4ec719812d39b1549740da52c429af375067fb9d
Yerbol Zhunussov <xexinkazahstan@mail.ru>
2022-06-17 01:56:54 +0600

    Added a link to repository

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
 @section('title') Home @endsection
 
 @section('main_content')
-  <p><i>Hello, please click Products</i> </p>
+  <p><i>Hello, please click "Products"</i> </p>
   <p>I have used <a href="https://github.com/alvin0/database-json-laravel" target="_blank">database-json-laravel</a> to store data in json file</p>
+  <p><i class="bi bi-github"></i>A repository for this project</p>
 @endsection
