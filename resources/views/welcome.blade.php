b17cfe0a3801cd26712d6b0633f40998b8379810
Yerbol Zhunussov <xexinkazahstan@mail.ru>
2022-06-17 02:06:21 +0600

    Updated message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,5 +4,5 @@
 @section('main_content')
   <p><i>Hello, please click "Products"</i> </p>
   <p>I have used <a href="https://github.com/alvin0/database-json-laravel" target="_blank">database-json-laravel</a> to store data in json file</p>
-  <p><i class="bi bi-github"></i>A repository for this project</p>
+  <p>A <a href="https://github.com/Erbolkz/LaravelSkillsTest" target="_blank">repository</a> for this project <i class="bi bi-github"></i> </p>
 @endsection
